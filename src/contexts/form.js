a7283fc5030e15287813d398f6fcdab58b252f53
wrodriguess77 <willrodrigues4@gmail.com>
2022-09-19 20:17:10 -0300

    Contexto global da aplicação

---
+++ src/contexts/form.js
@@ -0,0 +1,51 @@
+import { createContext, useState, useEffect } from 'react';
+import api from '../services/api';
+
+export const FormContext = createContext({});
+
+export function FormProvider({ children }) {
+    const [step, setStep] = useState(0);
+
+    const [scheduled, setScheduled] = useState({
+        day: '',
+        dayOfWeek: '',
+        month: 'Setembro',
+        year: '2022',
+        hour: '',
+    });
+
+    const [allSchedules, setAllSchedules] = useState([{}]);
+
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        telephone: ''
+    });
+
+    const [vehicle, setVehicle] = useState({});
+
+    useEffect(() => {
+        LoadVehicle(2);
+        LoadSchedules();
+    }, []);
+
+    async function LoadVehicle(id) {
+        await api.get(`/vehicle/read_one.php?id=${id}`)
+            .then(response => {
+                setVehicle(response.data);
+            })
+    }
+
+    async function LoadSchedules() {
+        await api.get('/scheduling/read.php')
+            .then(response => {
+                setAllSchedules(response.data);
+            })
+    }
+
+    return (
+        <FormContext.Provider value={{ user, setUser, step, setStep, scheduled, setScheduled, vehicle, setVehicle, allSchedules, setAllSchedules }}>
+            {children}
+        </FormContext.Provider>
+    )
+}
