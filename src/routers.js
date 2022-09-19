5b88b6ef355730ab0f35772c85667ed7166371e2
wrodriguess77 <willrodrigues4@gmail.com>
2022-09-19 20:19:29 -0300

    Removendo impotações da página excluida

--- src/routers.js
+++ src/routers.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
 import FormStep1 from './pages/FormStep1';
 import FormStep2 from './pages/FormStep2';
 import FormStep3 from './pages/FormStep3';
@@ -9,8 +8,6 @@ export default function Routers() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/home" exact element={<Home />} />
-
                 <Route path="/" element={<FormStep1 />} />
                 <Route path="/step2" element={<FormStep2 />} />
                 <Route path="/step3" element={<FormStep3 />} />
